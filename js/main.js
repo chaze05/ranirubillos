d196dfb9bc018b478cac9cf8b16c372787a1a1ef
priranirubillos@gmail.com <priranirubillos@gmail.com>
2018-03-16 23:36:36 +0800

    portfolio section

--- js/main.js
+++ js/main.js
@@ -1,53 +1,57 @@
-$(document).ready(function(){
+$(document).ready(function() {
     new Typed('#expertise', {
         strings: ['<strong>Front End Developer<strong> ', '<strong>Data Entry Specialist</strong> ', '<strong>Wordpress Developer</strong> ', '<strong>Web Scraping Expert</strong> ', '<strong>Lead Generation Expert</strong> '],
         typeSpeed: 50,
         backSpeed: 40,
         smartBackspace: true,
         loop: true
-      });
+    });
 
-    
+
     // show menu
-    $('.bar-container').on('click',function(){
+    $('.bar-container').on('click', function() {
         $('.menu-container').toggleClass('active');
     })
 
 
     //  skills animation
-    function runSkills(){
-        $('.progress-box').each(function(){
-        var myWidth = $(this).data('value');
-        $(this).find('.progress-bar').animate({'width':myWidth+"%"},2500);
+    function runSkills() {
+        $('.progress-box').each(function() {
+            var myWidth = $(this).data('value');
+            $(this).find('.progress-bar').animate({ 'width': myWidth + "%" }, 2500);
         })
     }
 
     $('.skills-section .container').viewportChecker({
-        callbackFunction:function(){
+        callbackFunction: function() {
             alert('visible')
         }
     });
 
     // scroll reveal
     window.sr = ScrollReveal();
-    sr.reveal('.section-header', {duration: '1000'})
-    sr.reveal('.skills-animation', {duration:'1500',
-    beforeReveal: function(){
-            $('.progress-box').each(function(){
+    sr.reveal('.section-header', { duration: '1000', mobile: true })
+    sr.reveal('.skills-animation', {
+        duration: '1500',
+        mobile: true,
+        beforeReveal: function() {
+            $('.progress-box').each(function() {
                 var myWidth = $(this).data('value');
-                $(this).find('.progress-bar').animate({'width':myWidth+"%"},2500);
+                $(this).find('.progress-bar').animate({ 'width': myWidth + "%" }, 2500);
             })
         }
     })
-    sr.reveal('.skills-delivered',{duration: '1500',
-    beforeReveal: function(){
-            $('.number').each(function(){
-                $(this).prop('counter',0).animate({
-                    counter : $(this).data('value')
-                },{
-                    duration : 4500,
+    sr.reveal('.skills-delivered', {
+        duration: '1500',
+        mobile: true,
+        beforeReveal: function() {
+            $('.number').each(function() {
+                $(this).prop('counter', 0).animate({
+                    counter: $(this).data('value')
+                }, {
+                    duration: 4500,
                     easing: 'swing',
-                    step: function(now){
+                    step: function(now) {
                         $(this).text(Math.ceil(now));
                     }
                 })
@@ -55,17 +59,17 @@ $(document).ready(function(){
             })
         }
     })
-    sr.reveal('.profile-info', {duration: '1500'})
-    sr.reveal('.myinterests', {duration: '1500'})
-    sr.reveal('li:nth-child(odd) .exp-box', { origin:'right', distance:'-1000px',duration: '1500'})
-    sr.reveal('li:nth-child(even) .exp-box', { origin:'left', distance:'-1000px',duration: '1500'})
+    sr.reveal('.profile-info', { duration: '1500', mobile: true })
+    sr.reveal('.myinterests', { duration: '1500', mobile: true })
+    sr.reveal('li:nth-child(odd) .exp-box', { mobile: true, origin: 'right', distance: '-555px', duration: '1500' })
+    sr.reveal('li:nth-child(even) .exp-box', { mobile: true, origin: 'left', distance: '-555px', duration: '1500' })
 
     // loader
-    setTimeout(function(){ 
+    setTimeout(function() {
         $('.page-overlay').css({
-            opacity:'0',
-            visibility:'hidden',
-            zIndex:'0'
+            opacity: '0',
+            visibility: 'hidden',
+            zIndex: '0'
         });
-    },500);
+    }, 500);
 });
